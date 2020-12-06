98777f19796ecf97417d75a340d617683f690566
Mateusz <mateusz.pytel@grupakmk.pl>
2020-12-06 23:32:47 +0100

    DotpayModuleState exported from module

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -2,3 +2,4 @@ export { DotpayPaymentPlugin } from './src/index';
 export { DotpayService } from './src/service/index';
 export { DotpayDao } from './src/dao/index';
 export { DotpayResponse, DotpayForm, DotpayStatus } from './src/types/index';
+export { DotpayModuleState } from './src/store/dotpay.default';
